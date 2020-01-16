c8056c491e39f357f13bdcfc443c75efc6f2ce5e
seanred360 <41050673+seanred360@users.noreply.github.com>
2020-01-16 13:39:53 +0800

    added gum game

---
+++ Assets/Gum Game/Scripts/Eater.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Eater : MonoBehaviour
+{
+    FoodPicker foodPicker;
+    int foodNumber;
+    public Transform selectedItem;
+    public GameObject gumSplatter;
+    public GameObject gumBubble;
+    GumGameManager gameManager;
+
+
+    void Start()
+    {
+        foodPicker = GameObject.FindObjectOfType<FoodPicker>();
+        gameManager = GameObject.FindObjectOfType<GumGameManager>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        foodNumber = collision.gameObject.GetComponent<Food>().foodNumber;
+        foodPicker.CheckAnswer(foodNumber);
+    }
+
+    public void PlayInflateSound()
+    {
+        GameObject.Find("AudioManager").GetComponent<AudioManager>().PlayMusic(2);
+        gumBubble.GetComponent<SpriteRenderer>().color = selectedItem.GetComponent<Image>().color;
+    }
+
+    public void PlayPopSound()
+    {
+        GameObject.Find("AudioManager").GetComponent<AudioManager>().PlaySFX(2);
+        gumSplatter.GetComponent<SpriteRenderer>().color = selectedItem.GetComponent<Image>().color;
+        if(gameManager.AreAllFoodsEaten())
+        {
+            StartCoroutine(StartGameOver());
+        }
+    }
+
+    IEnumerator StartGameOver()
+    {
+        yield return new WaitForSeconds(3f);
+        gameManager.GameOver();
+    }
+}
---
+++ Assets/Gum Game/Scripts/FoodPicker.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class FoodPicker : MonoBehaviour {
+
+    public int numberChosen;
+    GameObject soundSource;
+    AudioManager audioManager;
+    GumGameManager gameManager;
+    Eater eater;
+    public List<GameObject> uneatenFood;
+
+    // Use this for initialization
+    void Awake () {
+        eater = GameObject.FindObjectOfType<Eater>().GetComponent<Eater>();
+        gameManager = GameObject.Find("GameManager").GetComponent<GumGameManager>();
+        audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+        uneatenFood.AddRange(GameObject.FindGameObjectsWithTag("Food"));
+    }
+
+    void Start()
+    {
+        AssignFoodNumber();
+        PickNumber();
+    }
+
+    public void PickNumber()
+    {
+        if ((!gameManager.IsGameOver) && (uneatenFood.Count > 0))
+        {
+            Debug.Log("picking number");
+            numberChosen = Random.Range(0, uneatenFood.Count);
+            uneatenFood[numberChosen].GetComponent<AudioSource>().Play();
+        }
+    }
+
+    public void CheckAnswer(int useIndex)
+    {
+        if (numberChosen == useIndex)
+        {
+            SelectTrue(useIndex);
+        }
+        else
+        if (numberChosen != useIndex)
+        {
+            SelectFalse(useIndex);
+        }
+    }
+
+    public void SelectTrue(int useIndex)
+    {
+        eater.GetComponent<Animator>().Play("ChewGum");
+        uneatenFood[useIndex].GetComponent<Food>().isEaten = true;
+        uneatenFood.RemoveAt(useIndex);
+        AssignFoodNumber();
+        Invoke("PickNumber",7);
+        //PickNumber();
+        Debug.Log("Correct");
+        audioManager.PlaySFX(0);
+    }
+
+    public void SelectFalse(int useIndex)
+    {
+        eater.GetComponent<Animator>().Play("Refuse");
+        Debug.Log("Wrong");
+        gameManager.GetComponent<Mover>().Move(uneatenFood[useIndex]);
+        audioManager.PlaySFX(1);
+    }
+
+    void AssignFoodNumber()
+    {
+        for (int i = 0; i < uneatenFood.Count; i++)
+        {
+            if (uneatenFood[i].name == uneatenFood[i].name)
+            {
+                uneatenFood[i].GetComponent<Food>().foodNumber = i;
+            }
+        }
+    }
+}
