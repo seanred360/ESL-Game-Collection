eef771ef4a44964bc9399db662516ac9d2235681
seanred360 <41050673+seanred360@users.noreply.github.com>
2020-01-19 08:07:08 +0800

    1-18

---
+++ Assets/LetterWordSweepGame/Scripts/PhonicsLettersController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PhonicsLettersController : MonoBehaviour
+{
+    public List<GameObject> wordObjects;
+    public AudioManager audioManager;
+    bool canPlaySFX = true;
+    public GameObject touchParticle;
+    public GameObject letters;
+    public GameObject endScreen;
+    public AudioManager endSounds;
+
+    private void Start()
+    {
+        audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+        wordObjects.AddRange(GameObject.FindGameObjectsWithTag("Umbrella"));
+    }
+
+    private void Update()
+    {
+        if(wordObjects.Count <=  0 && canPlaySFX)
+        {
+            audioManager.PlaySFX(1);
+            GameObject.Find("Big Letter").GetComponent<Image>().color = new Color(0f,.72f,1f,1f);
+            GameObject.Find("Small Letter").GetComponent<Image>().color = new Color(0f, .72f, 1f, 1f);
+            letters.GetComponent<Animator>().Play("LetterDance");
+            StartCoroutine(OpenEndScreen());
+            canPlaySFX = false;
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector3 p = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+            Instantiate(touchParticle, new Vector3(p.x, p.y, 0.0f), Quaternion.identity);
+
+        }
+    }
+
+    IEnumerator OpenEndScreen()
+    {
+        yield return new WaitForSeconds(5.3f);
+        endScreen.SetActive(true);
+        endSounds.PlayMusic(0);
+        endSounds.PlaySFX(Random.Range(0, 2));
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/LetterWordSweepGame/Scripts/Umbrella.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class Umbrella : MonoBehaviour
+{
+    Vector2 endMove;
+    PhonicsLettersController gameManager;
+
+    //public void OnPointerClick(PointerEventData umbrella)
+    //{
+    //    if (GetComponent<Button>().interactable == true)
+    //    {
+    //        audioSource.Play();
+    //        endMove.x = Random.Range(10, 30);
+    //        endMove.y = Random.Range(10, 30);
+    //        transform.DOMove(endMove, 1f).OnComplete(DestroyThis);
+    //    }
+    //}
+
+    private void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<PhonicsLettersController>();
+    }
+
+    private void OnMouseDown()
+    {
+        gameManager.audioManager.PlaySFX(0);
+        gameManager.audioManager.PlaySFX(2);
+        endMove.x = 20 * Random.Range(-1,1);
+        endMove.y = 20 * Random.Range(-1, 1);
+        if (endMove == Vector2.zero) endMove = new Vector2(10, 10);
+        Debug.Log(endMove);
+        transform.DOMove(endMove, 2f).OnComplete(DestroyThis);
+        transform.DORotate(new Vector3(0, 0, 359), 1f);
+    }
+
+    void DestroyThis()
+    {
+        gameManager.wordObjects.Remove(gameObject);
+        Destroy(gameObject);
+    }
+}
