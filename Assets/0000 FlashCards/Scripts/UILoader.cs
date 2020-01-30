a9612d8b15d31bd8000d2ea5095a7b1f1bcfb945
seanred360 <41050673+seanred360@users.noreply.github.com>
2020-01-30 16:53:03 +0800

    changed to scence based ui

--- Assets/0000 FlashCards/Scripts/UILoader.cs
+++ Assets/0000 FlashCards/Scripts/UILoader.cs
@@ -12,48 +12,15 @@ public class UILoader : MonoBehaviour
     public void ToggleOptionsUI()
     {
         if (SceneManager.GetSceneByName("OptionsUI").isLoaded == false)
+        {
             SceneManager.LoadSceneAsync("OptionsUI", LoadSceneMode.Additive);
+            Time.timeScale = 0;
+        }
+
         else
+        {
             SceneManager.UnloadSceneAsync("OptionsUI");
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.F1))
-        {
-            if (SceneManager.GetSceneByName("OptionsUI").isLoaded == false)
-                SceneManager.LoadSceneAsync("OptionsUI", LoadSceneMode.Additive);
-            else
-                SceneManager.UnloadSceneAsync("OptionsUI");
+            Time.timeScale = 1;
         }
-
-        if (Input.GetKeyDown(KeyCode.F3))
-        {
-            if (SceneManager.GetSceneByName("Level2").isLoaded)
-                SceneManager.UnloadSceneAsync("Level2");
-
-            if (SceneManager.GetSceneByName("Level1").isLoaded == false)
-            {
-                SceneManager.LoadSceneAsync("Level1", LoadSceneMode.Additive).completed += operation => 
-                    SceneManager.SetActiveScene(SceneManager.GetSceneByName("Level1"));
-            }
-        }
-        
-        if (Input.GetKeyDown(KeyCode.F4))
-        {
-            if (SceneManager.GetSceneByName("Level1").isLoaded)
-                SceneManager.UnloadSceneAsync("Level1");
-
-            if (SceneManager.GetSceneByName("Level2").isLoaded == false)
-            {
-                SceneManager.LoadSceneAsync("Level2", LoadSceneMode.Additive)
-                    .completed += HandleLevel2LoadCompleted;
-            }
-        }
-    }
-
-    private void HandleLevel2LoadCompleted(AsyncOperation obj)
-    {
-        SceneManager.SetActiveScene(SceneManager.GetSceneByName("Level2"));
     }
 }
